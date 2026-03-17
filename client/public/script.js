80b8237da70ce4b7a19a56e216271d28f4e6e039
cf2031293 <chenf1@ufl.edu>
2026-03-17 14:11:36 -0400

    Delete client/public/script.js

--- client/public/script.js
+++
@@ -1,8 +0,0 @@
-function loadData() {
-  fetch("/api/data")
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById("output").innerText =
-        JSON.stringify(data, null, 2);
-    });
-}
