c97de843760f3a08f8ffd99e26c873d262d46ca4
TLeaCS <kaleathompson04@gmail.com>
2026-03-24 18:01:53 -0400

    reset to main files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
-import {BrowserRouter,Routes, Route}from'react-router-dom'
-import Login from'./src/diffPages/Login'
-import Home from'./src/diffPages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./src/diffPages/Login.jsx";
+import Home from "./src/diffPages/Home_Page.jsx";
+import Dashboard from "./src/diffPages/Dashboard.jsx";
+import Study from "./src/diffPages/Study_Page.jsx";
+import Create from "./src/diffPages/Create_Edit_Card.jsx";
+
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/"element={<Login />}/>
-    <Route path="/home" element={<Home />}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/study" element={<Study />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/src/diffPages/Login.jsx
+++ client/src/src/diffPages/Login.jsx
@@ -1,27 +1,30 @@
-import {useNavigate} from 'react-router-dom'
 export default function Login() {
-const nav= useNavigate()
-return(
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="bg-white rounded-2xl shadow-md p-8 w-full max-w-sm">
-        <h2 className="text-xl font-bold mb-6">Login</h2>
+  return (
+    <div className= "min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className= "bg-white rounded-2xl shadow-md p-8 w-full max-w-sm">
+        <h2 className= "text-xl font-bold mb-6">Login</h2>
         {/* This is the email part I did*/}
-        <input type="email"placeholder="Email"
-        className="w-full border border-gray-200 rounded-lg px-4 py-3 mb-4 text-sm outline-none focus:border-red-400"
+        <input
+          type= "email"
+          placeholder= "Email"
+          className= "w-full border border-gray-200 rounded-lg px-4 py-3 mb-4 text-sm outline-none focus:border-red-400"
         />
         {/* This is the password I did*/}
-        <input type="password"placeholder="Password"
-        className="w-full border border-gray-200 rounded-lg px-4 py-3 mb-6 text-sm outline-none focus:border-red-400"
+        <input
+          type= "password"
+          placeholder= "Password"
+          className= "w-full border border-gray-200 rounded-lg px-4 py-3 mb-6 text-sm outline-none focus:border-red-400"
         />
         {/* This is the submit button I did */}
-        <button onClick={()=>nav('/home')}
-        className="w-full bg-red-400 hover:bg-red-500 text-white py-3 rounded-lg text-sm font-medium">
-        Login</button>
+        <button className= "w-full bg-red-400 hover:bg-red-500 text-white py-3 rounded-lg text-sm font-medium">
+          Login
+        </button>
         {/* This is the link to signup I did*/}
         <p className="text-center text-sm text-gray-500 mt-4">
-        Dont have an account?{" "}
-        <span className="text-red-400 cursor-pointer hover:underline">
-        Sign up</span>
-      </p>
+          Dont have an account?{" "}
+          <span className= "text-red-400 cursor-pointer hover:underline">Sign up</span>
+        </p>
+      </div>
     </div>
-    </div>)}
+  )
+}
